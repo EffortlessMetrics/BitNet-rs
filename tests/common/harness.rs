fd0e93de7dbf122721dfa0d86706c4b5a28c2420
Steven Zimmerman <git@effortlesssteven.com>
2025-09-23 00:57:05 -0400

    fix(harness): pass fixture context to test case setup method

--- tests/common/harness.rs
+++ tests/common/harness.rs
@@ -268,8 +268,7 @@ impl TestHarness {
 
         // Call the test's setup method with stable API
         {
-            self.fixtures.ctx();
-            test_case.setup(()).await?;
+            test_case.setup(self.fixtures.ctx()).await?;
         }
 
         Ok(())
@@ -440,8 +439,7 @@ impl TestHarnessClone {
 
         // Call the test's setup method with stable API
         {
-            self.fixtures.ctx();
-            test_case.setup(()).await?;
+            test_case.setup(self.fixtures.ctx()).await?;
         }
 
         Ok(())
