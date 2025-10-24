82517b137b8de99e746e1ad59a4ce955fc81b7f1
Steven Zimmerman <git@effortlesssteven.com>
2025-10-24 00:21:24 -0400

    c

--- tests-new/archive/demo_1e6_tolerance.rs
+++
@@ -1,7 +0,0 @@
-#![cfg(feature = "integration-tests")]
-// Include the tolerance validation code
-include!("test_1e6_tolerance_validation_final.rs");
-
-fn main() {
-    demonstrate_1e6_tolerance_validation();
-}
